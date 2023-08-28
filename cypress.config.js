52e12310668029ebb0cbb1acc19b3d52239ab872
Alisson Gabriel Dal Bosco <alisson.gabriel5@hotmail.com>
2023-08-28 20:17:05 -0300

    first commit

--- cypress.config.js
+++ cypress.config.js
@@ -1,14 +1,22 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'cakrza',
   e2e: {
+    reporter: 'cypress-mochawesome-reporter',
+    reporterOptions: {
+      charts: true,
+      reportTitle: 'Projeto do curso de Cypress',
+      reportPageTitle: 'Projeto do curso de Cypress'
+    },
     baseUrl: 'https://automationpratice.com.br',
     defaultCommandTimeout: 5000,
     viewportWidth: 1920,
     viewportHeight: 1080,
 
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
+
     },
   },
 });
